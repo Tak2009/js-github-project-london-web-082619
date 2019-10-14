838464250d6d3689cbf29d3e654307be3a0bc756
github username <github email address>
2019-10-14 23:55:06 +0100

    changed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,33 @@
 
-//get data 
-API.getUsers(userName)
-.then(users => renderUsers(users));
+const userList = document.querySelector("#user-list");
+const gitForm = document.querySelector("#github-form");
+
+//add event to submit button
+gitForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    API.getUsers(gitForm.search.value)//getUsersをコール
+    .then(users => renderUsers(users));//受け取った後の.then. 
+});
+
 
 //control itration
-
 const renderUsers = function(users){
-    for (const user in )
+　　　for (const user of users.items) {//in{Hash}ではない。items:以降は[Array]に
+        renderUser(user);//userオブジェクトが各エレメントとして入っている. Dogと比較すること
+    }
+};
+
+//render user
+const renderUser = function(user){
+    const div = document.createElement("div");
+    div.id = user.id;
+    const li = document.createElement("li");
+    li.id = user.login;
+    li.innerText = user.login;
+    const img = document.createElement("img");
+    img.src = user.avatar_url
+    const p = document.createElement("p");
+    p.innerText = user.url;
+    div.append(li,img,p);
+    userList.appendChild(div);
 }
